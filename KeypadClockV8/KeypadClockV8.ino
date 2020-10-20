75f73724d9c90c1c917adac2a67f6798d71fcb8e
NicholasRC <63579737+NicholasRC@users.noreply.github.com>
2020-10-20 14:33:41 -0400

    FOR NEW ARDUINO
    THIS IS V7 ,
    but pins are changed for the new arduino.
    but firebase is included

---
+++ KeypadClockV8/KeypadClockV8.ino
@@ -0,0 +1,249 @@
+#include <Firebase_Arduino_WiFiNINA_HTTPClient.h>
+#include <Firebase_Arduino_WiFiNINA.h>
+
+//KeypadClockV7.ino
+
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1331.h>
+#include <SPI.h>
+#include <Keypad.h>
+#include <EEPROM.h>
+#include "menu.h" //contains Menu::
+//RTC CLOCK
+#include "RTClib.h"
+#include <Wire.h>
+#include "ds3231.h"
+//#include "Firebase_Arduino_WiFiNINA.h"
+
+//Firebase defines
+#define FIREBASE_HOST "pill-alarm-be539.firebaseio.com"
+#define FIREBASE_AUTH "UaIlUqx6ZnV0xkLNyIkSeOD6dHDD3Xm3KoKM2r0O"
+
+
+//CALL THE ANALOG PINS AS DIGITAL: call them [Pin 14 .. Pin 19]
+
+//KEYPAD
+char customKey;
+const byte ROWS = 4;
+const byte COLS = 3;
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+
+// digital connection Pins to the arduino for the keypad
+//PIN LOCATIONS UPDATE//PIN LOCATIONS UPDATE//PIN LOCATIONS UPDATE//PIN LOCATIONS UPDATE//PIN LOCATIONS UPDATE
+byte rowPins[ROWS] = {7, 6, 5, 4};
+byte colPins[COLS] = {3, 2, 1};
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+#define SerialDebugging true
+
+///////////////////DISPLAY///////////////////////////////
+//PIN LOCATIONS UPDATE//PIN LOCATIONS UPDATE//PIN LOCATIONS UPDATE//PIN LOCATIONS UPDATE//PIN LOCATIONS UPDATE
+// The SSD1331 is connected like this (plus VCC plus GND)
+const uint8_t OLED_pin_scl_sck = 12;  //12 old
+const uint8_t OLED_pin_sda_mosi = 11; //11 old
+const uint8_t OLED_pin_cs_ss = 10;
+const uint8_t OLED_pin_res_rst = 9;
+const uint8_t OLED_pin_dc_rs = 8;
+
+// SSD1331 color definitions
+const uint16_t OLED_Color_Black = 0x0000;
+const uint16_t OLED_Color_Blue = 0x001F;
+const uint16_t OLED_Color_Red = 0xF800;
+const uint16_t OLED_Color_Green = 0x07E0;
+const uint16_t OLED_Color_Cyan = 0x07FF;
+const uint16_t OLED_Color_Magenta = 0xF81F;
+const uint16_t OLED_Color_Yellow = 0xFFE0;
+const uint16_t OLED_Color_White = 0xFFFF;
+
+// The colors we actually want to use
+ uint16_t OLED_Text_Color = OLED_Color_Green; //declared below under the creation of clkMenu
+uint16_t OLED_Backround_Color = OLED_Color_Black;
+
+// Menu Line positions
+const uint8_t menu1X = 0;   //menu line 1
+const uint8_t menu1Xtab = 4;//menu line 1 indent
+const uint8_t menu1Y = 0;  //menu line 1
+const uint8_t menu2X = 0;   //menu line 2
+const uint8_t menu2Y = 8;  //menu line 2
+const uint8_t menu4X = 0;   //menu line 4
+const uint8_t menu4Y = 24;  //menu line 4
+const uint8_t menu3X = 0;
+const uint8_t menu3Y = 16;  //menu line 3
+const uint8_t menu5Y = 32;  //menu line 5
+const uint8_t menu6Y = 40;  //menu line 6
+const uint8_t menu7Y = 48;  //menu line 7
+const uint8_t menu8Y = 56;  //menu line 8
+
+//location of HH:MM for time Menus
+const uint8_t hour1X = 0;  //menu clock input
+const uint8_t hour1Y = 16; //menu clock input
+const uint8_t hour2X = 6;  //menu clock input
+const uint8_t hour2Y = 16; //menu clock input
+const uint8_t min1X = 18;  //menu clock input
+const uint8_t min1Y = 16;  //menu clock input
+const uint8_t min2X = 24;  //menu clock input
+const uint8_t min2Y = 16;  //menu clock input
+
+//////WiFi VARS//////////////////////////////////////
+String inputString1 = ""; //temp storage
+String inputString2 =""; //temp storage
+uint16_t abLine = 0;
+char alphabet[] = {'a','b','c' ,'d','e','f' ,'g','h','i',
+                   'j','k','l' ,'m','n','o' ,'p','q','r',
+                   's','t','u' ,'v','w','x' ,'y','z',' ',
+                   'A','B','C' ,'D','E','F' ,'G','H','I',
+                   'J','K','L' ,'M','N','O' ,'P','Q', 'R',
+                   'S','T' ,'U','V','W','X' ,'Y','Z',' ',
+                   '0','1','2' ,'3','4','5' ,'6','7','8',
+                   '9','!','@' ,'#','$','%' ,'^','&','*',
+                   '(',')','-' ,'_','=','+' ,',','?','.',
+                   '<','>','\'','"','\\','/','|',':',';',
+                   '[',']' ,'{','}' ,'`','~',' ',' ',' '};
+
+// assume the display is off until configured in setup()
+bool isDisplayVisible = false;
+// declare size of working string buffers. Basic strlen("d hh:mm:ss") = 10
+const size_t MaxString = 16;
+// the string being displayed on the SSD1331 (initially empty)
+char oldTimeString[MaxString] = { 0 };
+// the interrupt service routine affects this
+volatile bool isButtonPressed = false;
+
+
+String menu1Text = "1:Time\n2:Pill Alarm\n3:Alarm\n4:Child Safety\n5:Change PIN\n6:WiFi\n7:Pills Replaced\n8:Settings";
+String PINin = "";
+volatile uint16_t hourIn = 0;
+volatile uint16_t minIn = 0;
+volatile uint16_t ampmIn = 0; //0:not set 1:AM 2:PM
+
+///////////////////////RTC VAR///////////////////
+//rtc clock times
+uint8_t hourOld;
+uint8_t minOld;
+
+// rtc clock location
+const uint8_t RTChour1X = 4;  //menu clock input
+const uint8_t RTChour1Y = 11; //menu clock input
+const uint8_t RTChour2X = 22;  //menu clock input
+const uint8_t RTChour2Y = 11; //menu clock input
+const uint8_t RTCmin1X = 58;  //menu clock input
+const uint8_t RTCmin1Y = 11;  //menu clock input
+const uint8_t RTCmin2X = 76;  //menu clock input
+const uint8_t RTCmin2Y = 11;  //menu clock input
+const uint8_t RTCampmX = 15;
+const uint8_t RTCampmY = 34;
+
+/////////////////ALARM VARS//////////////////////////
+//helper variable to set state of the alarm to allow loops without using a loop that will freeze the program
+uint8_t pillAlarmState = 0;
+uint8_t alarmState = 0;
+uint8_t pillAlarmTripped = 0; //for pillAlarm()
+uint8_t alarmTripped = 0; //for alarm()
+
+//////////////DECLARATIONS OF CLASSES//////////////////////////
+// declare the display
+Adafruit_SSD1331 oled = Adafruit_SSD1331(OLED_pin_cs_ss,OLED_pin_dc_rs,OLED_pin_sda_mosi,OLED_pin_scl_sck,OLED_pin_res_rst);
+//declare the menu
+Menu clkMenu;
+//declare the clock
+RTC_DS3231 rtc;
+DateTime now;
+
+//Define Firebase data object
+FirebaseData firebaseData;
+
+//END GLOBAL VARIABLES ///////////////////////////////////////////
+
+// interrupt service routine
+void senseButtonPressed() {
+    if (!isButtonPressed) {isButtonPressed = true;}
+}
+//FUNCTIONS
+void keypadMenu();
+void rtcTime(uint8_t in);
+uint8_t pillAlarm();
+uint8_t alarm();
+
+void setup()
+{
+//rtcTime
+rtc.begin();
+// settling time for the rtc
+delay(750);
+
+// ignore any power-on-reboot garbage
+isButtonPressed = false;
+
+// initialise the SSD1331
+oled.begin();
+oled.setFont();
+oled.fillScreen(OLED_Backround_Color);
+oled.setTextColor(clkMenu.getColor());
+oled.setTextSize(3);
+
+// the display is now on
+isDisplayVisible = true;
+
+//WORKING
+RTC_DS3231 rtc;
+DateTime now = rtc.now();
+hourOld = now.hour();
+minOld = now.minute();
+
+//load variables into clkMenu, Alarms, Pill counter, PIN, WiFi, Time Format, Menu Color, Child Safety
+clkMenu.load();
+OLED_Text_Color = clkMenu.getColor();
+oled.fillScreen(OLED_Backround_Color);
+rtcTime(1);
+}// END SETUP
+
+
+void loop()
+{
+//keypad
+if((pillAlarmTripped == 0) && (alarmTripped == 0)){keypadMenu();}   //Skip over to allow input into Alarm Functions
+
+//RTC CLOCK TIME
+rtcTime(0);
+
+  String WIFI_SSID = clkMenu.getSSID();
+  const char* w_SSID = WIFI_SSID.c_str();
+  String WIFI_PASSWORD = clkMenu.getPASS();
+  const char* PASSWORD = WIFI_PASSWORD.c_str();
+// Serial.print("Connecting to Wi-Fi");
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED)
+  {
+    status = WiFi.begin(w_SSID, PASSWORD);
+    Serial.print(".");
+    delay(300);
+  }
+
+  while (status == WL_CONNECTED) {
+    //put in wifi signal picture
+  }
+
+//CALL THE PILL ALARM FUNCTION
+// check if the trigger time for the pill alarm is the current time
+if( (clkMenu.getPillHH() == now.hour() && clkMenu.getPillMM() == now.minute() && now.second() == 0) || (pillAlarmTripped == 1) ) {pillAlarmTripped = pillAlarm();}
+
+//CALL THE ALARM FUNCTION
+// check if the trigger time for the pill alarm is the current time
+if( (clkMenu.getAlarmHH() == now.hour() && clkMenu.getAlarmMM() == now.minute() && now.second() == 0) || (alarmTripped == 1) ||
+    (clkMenu.snoozeAlarmHH == now.hour() && clkMenu.snoozeAlarmMM == now.minute() && now.second() == 0) )
+  {
+    // Check if the Pill Alarm is Active, INACTIVE: go into alarm, ACTIVE, delay until Pill Alarm is complete
+    if(pillAlarmTripped == 0){alarmTripped = alarm();}
+    else alarmTripped = 1;
+  }
+
+
+// shorten if too much latency
+//delay(100);
+}//END LOOP
---
+++ KeypadClockV8/RTC.ino
@@ -0,0 +1,476 @@
+//RTC.ino
+
+ void rtcTime(uint8_t in)
+{
+   
+   now = rtc.now();
+
+   if(clkMenu.getClkON()) // This will skip this code when in a menu
+     {
+     //CHECK IF OUT OF PILLS
+     if(clkMenu.getPillCounter() == 7){oled.setTextColor(OLED_Color_Red);}
+     else                             {oled.setTextColor(OLED_Backround_Color);}
+     // if pill counter == 7 print in red else print in backround
+     oled.setTextSize(1);
+     oled.setCursor(menu1X,menu1Y);
+     oled.print("OUT OF PILLS");
+     }
+   
+   oled.setTextSize(3);
+   oled.setTextColor(OLED_Text_Color);
+   if(in == 1)
+   {
+    oled.setCursor(RTChour1X,RTChour1Y);
+    if(clkMenu.getTimeFormat() == 24)
+      {
+      if(now.hour() < 10){oled.print('0');}
+      oled.print(now.hour());
+      }
+    else //12 hour format
+      {
+      //1-9 am
+      if((now.hour() > 0) && (now.hour() < 10))       { oled.setCursor(RTChour1X,RTChour1Y); oled.print('0'); oled.print(now.hour(), DEC);}
+      //1-9 pm
+      else if((now.hour() > 12) && (now.hour() < 22)) { oled.setCursor(RTChour1X,RTChour1Y); oled.print('0'); oled.print(now.hour() - 12, DEC);}
+      //10am-Noon (
+      else if((now.hour() > 9) && (now.hour() < 13))  {oled.setCursor(RTChour1X,RTChour1Y);oled.print((now.hour()), DEC);}
+      //10pm-midnight
+      else if((now.hour() > 21) && (now.hour() < 24)) {oled.setCursor(RTChour1X,RTChour1Y);oled.print(now.hour() - 12, DEC);}
+      else if(now.hour() == 0)                          {oled.setCursor(RTChour1X,RTChour1Y);oled.print("12");}
+      //AM PM
+      if((now.hour() >= 0) && (now.hour() < 12)){oled.setTextSize(2);oled.setCursor(RTCampmX,RTCampmY);oled.print("AM");oled.setTextSize(3);} //AM
+      else                                      {oled.setTextSize(2);oled.setCursor(RTCampmX,RTCampmY);oled.print("PM");oled.setTextSize(3);} //PM
+      }
+
+    oled.setCursor(40,11);
+    oled.print(':');
+    oled.setCursor(RTCmin1X,RTCmin1Y);
+    if(now.minute() < 10){oled.print('0');}
+    oled.print(now.minute());
+    //update hourOld and minOld, for when you are in menu and the min/hour changes
+    hourOld = now.hour();
+    minOld = now.minute();
+   }
+   else //update
+   {
+   if(clkMenu.getClkON()) // This will skip this code when in a menu
+   {
+     oled.setCursor(40,11); oled.print(':'); // This is the :, HH:MM, it doesn't need to be printed elsewhere
+
+// CHECK AND UPDATE THE HOUR
+     if(hourOld != now.hour())
+     {
+       oled.setTextColor(OLED_Backround_Color); //Set Clear hourOld
+       if(clkMenu.getTimeFormat() == 24)
+       {
+        //CLEAR hourOld 24
+        if(hourOld < 9)         {oled.setCursor(RTChour2X,RTChour2Y);oled.print(hourOld);}
+        else if(hourOld == 9)   {oled.setCursor(RTChour1X,RTChour1Y);oled.print('0');oled.print(hourOld);}
+        else if(hourOld < 19)   {oled.setCursor(RTChour2X,RTChour2Y);oled.print((hourOld - 10));}
+        else if(hourOld == 19)  {oled.setCursor(RTChour1X,RTChour1Y);oled.print(hourOld);}
+        else if (hourOld < 23)  {oled.setCursor(RTChour2X,RTChour2Y);oled.print((hourOld - 20));}
+        else /* hourOld == 23*/ {oled.setCursor(RTChour1X,RTChour1Y);oled.print(hourOld);}
+
+        oled.setTextColor(OLED_Text_Color);
+        //PRINT now.hour(), DEC 24
+        if(hourOld < 9)         {oled.setCursor(RTChour2X,RTChour2Y);oled.print(now.hour(), DEC);}
+        else if(hourOld == 9)   {oled.setCursor(RTChour1X,RTChour1Y);oled.print(now.hour(), DEC);}
+        else if(hourOld < 19)   {oled.setCursor(RTChour2X,RTChour2Y);oled.print((now.hour() - 10), DEC);}
+        else if(hourOld == 19)  {oled.setCursor(RTChour1X,RTChour1Y);oled.print(now.hour(), DEC);}
+        else if (hourOld < 23)  {oled.setCursor(RTChour2X,RTChour2Y);oled.print((now.hour() - 20), DEC);}
+        else /* hourOld == 23*/ {oled.setCursor(RTChour1X,RTChour1Y);oled.print('0');oled.print(now.hour(), DEC);}
+
+       }//end 24 hour
+
+       else if(clkMenu.getTimeFormat() == 12) //12 hour format
+       {
+        //CLEAR hourOld 12
+        //1-8 am and pm
+        if((hourOld > 0) && (hourOld < 9))        {oled.setCursor(RTChour2X,RTChour2Y);oled.print(hourOld);}
+        else if((hourOld > 12) && (hourOld < 21)) {oled.setCursor(RTChour2X,RTChour2Y);oled.print(hourOld - 12);}
+        //midnight and noon
+        else if((hourOld == 0)||(hourOld == 12) ) {oled.setCursor(RTChour1X,RTChour1Y);oled.print("12");}
+        //9am and 9pm
+        else if((hourOld == 9)||(hourOld == 21) ) {oled.setCursor(RTChour1X,RTChour1Y);oled.print("09");}
+        //10 and 11 am and pm
+        else if((hourOld > 9) && (hourOld < 12))  {oled.setCursor(RTChour2X,RTChour2Y);oled.print((hourOld - 10));}
+        else /* hourOld == 22 or 23 */            {oled.setCursor(RTChour2X,RTChour2Y);oled.print((hourOld - 22));}
+
+        //Remove the AM/PM indicator
+        //remove AM if hourOld == 11
+        if(hourOld == 11){oled.setTextSize(2);oled.setCursor(RTCampmX,RTCampmY);oled.print('A');oled.setTextSize(3);}
+        //remove PM if hourOld == 23
+        else if(hourOld == 23){oled.setTextSize(2);oled.setCursor(RTCampmX,RTCampmY);oled.print('P');oled.setTextSize(3);}
+
+        oled.setTextColor(OLED_Text_Color);
+        //PRINT now.hour(), DEC 12
+        //1-8 am and pm
+        if((hourOld > 0) && (hourOld < 9))        {oled.setCursor(RTChour2X,RTChour2Y);oled.print(now.hour());}
+        else if((hourOld > 12) && (hourOld < 21)) {oled.setCursor(RTChour2X,RTChour2Y);oled.print(now.hour() - 12);}
+        //midnight and noon
+        else if((hourOld == 0)||(hourOld == 12) ) {oled.setCursor(RTChour1X,RTChour1Y);oled.print("01");}
+        //9am and 9pm
+        else if((hourOld == 9)||(hourOld == 21) ) {oled.setCursor(RTChour1X,RTChour1Y);oled.print("10");}
+        //10 and 11 am and pm
+        else if((hourOld > 9) && (hourOld < 12))  {oled.setCursor(RTChour2X,RTChour2Y);oled.print((now.hour() - 10));}
+        else if((hourOld == 23))                  {oled.setCursor(RTChour2X,RTChour2Y);oled.print('2');}
+        else /* hourOld == 22*/                   {oled.setCursor(RTChour2X,RTChour2Y);oled.print('1');}
+
+        //ADD the AM/PM indicator
+        //Add AM if hour == 0, hour old == 23
+        if(hourOld == 23) {oled.setTextSize(2);oled.setCursor(RTCampmX,RTCampmY);oled.print('A');oled.setTextSize(3);}
+        //Add PM if hour == 12 , hourold == 11
+        else if(hourOld == 11){oled.setTextSize(2);oled.setCursor(RTCampmX,RTCampmY);oled.print('P');oled.setTextSize(3);}
+
+       }//end 12 hour
+
+       //update hourOld
+       if(hourOld < 23){hourOld += 1;} else{hourOld = 0;}
+     }//END UPDATE HOUR
+
+// CHECK AND UPDATE MINUTES
+       oled.setCursor(RTCmin1X,RTCmin1Y);
+       if(minOld != now.minute())
+       {
+        //CLEAR minOld
+        oled.setTextColor(OLED_Backround_Color);
+
+        if(minOld < 9)         {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print(minOld);}
+        else if(minOld == 9)   {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print('0');oled.print(minOld);}
+        else if(minOld < 19)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((minOld - 10));}
+        else if(minOld == 19)  {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(minOld);}
+        else if(minOld < 29)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((minOld - 20));}
+        else if(minOld == 29)  {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(minOld);}
+        else if(minOld < 39)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((minOld - 30));}
+        else if(minOld == 39)  {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(minOld);}
+        else if(minOld < 49)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((minOld - 40));}
+        else if(minOld == 49)  {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(minOld);}
+        else if(minOld < 59)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((minOld - 50));}
+        else/* minOld == 59 */ {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(minOld);}
+
+        //print (now.minute(), DEC);
+        oled.setTextColor(OLED_Text_Color);
+
+        if(minOld < 9)         {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print(now.minute(), DEC);}
+        else if(minOld == 9)   {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(now.minute(), DEC);}
+        else if(minOld < 19)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((now.minute() - 10), DEC);}
+        else if(minOld == 19)  {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(now.minute(), DEC);}
+        else if(minOld < 29)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((now.minute() - 20), DEC);}
+        else if(minOld == 29)  {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(now.minute(), DEC);}
+        else if(minOld < 39)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((now.minute() - 30), DEC);}
+        else if(minOld == 39)  {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(now.minute(), DEC);}
+        else if(minOld < 49)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((now.minute() - 40), DEC);}
+        else if(minOld == 49)  {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print(now.minute(), DEC);}
+        else if(minOld < 59)   {oled.setCursor(RTCmin2X,RTCmin2Y);oled.print((now.minute() - 50), DEC);}
+        else/* minOld == 59 */ {oled.setCursor(RTCmin1X,RTCmin1Y);oled.print('0');oled.print(now.minute(), DEC);}
+
+         //update minOld
+         if(minOld < 59){minOld += 1;} else{minOld = 0;}
+       }//END UPDATE MIN
+   }//END Checks if Display Clock is ON
+   }//update
+}//RTC TIME
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ALARMS
+
+//have these as variable of menu class, public variables
+//have them set when the alarm time is set, same function call
+uint8_t snoozeAlarmHH = clkMenu.getAlarmHH();
+uint8_t snoozeAlarmMM = clkMenu.getAlarmMM();
+
+
+
+uint8_t pillAlarm()
+{
+  customKey = customKeypad.getKey();
+  OLED_Text_Color = OLED_Color_Red; //red for alarm
+  if((now.minute() == clkMenu.getAlarmMM()) || (now.minute() == clkMenu.snoozeAlarmMM)){rtcTime(1);} else{rtcTime(0);}
+  uint8_t AlarmTripped = 1; //0 for false 1 for true
+  switch(pillAlarmState)
+  {
+    case 0:
+    // Turn on LEDS that shine on the cup
+    // DAVID INSERT HERE
+
+    //Turn on the Speaker
+    // DAVID INSERT HERE
+
+    if(clkMenu.getChildSafety() == 1)
+    {
+      oled.setTextSize(1);
+      oled.setTextColor(OLED_Text_Color);
+      oled.setCursor(menu1X, menu7Y);
+      oled.print("Enter PIN then *");
+      PINin = "";
+      pillAlarmState = 1;  
+    }
+    else {pillAlarmState = 2; goto CASE2;}
+    // NO break; to allow fall through
+
+    case 1:
+    //checks shild safety on/OFF
+    //if on get PIN INPUT
+      oled.setTextSize(1);
+
+      // NON WORKING CASE 1
+
+switch (customKey)
+      {
+        case '1':
+          PINin += '1';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+        break; //case '1'
+
+        case '2':
+          PINin += '2';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+          break; //case '2'
+
+        case '3':
+          PINin += '3';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+          break; //case '3'
+
+        case '4':
+          PINin += '4';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+          break; //case '4'
+
+        case '5':
+          PINin += '5';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+          break; //case '5'
+
+        case '6':
+          PINin += '6';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+          break; //case '6'
+
+        case '7':
+          PINin += '7';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+          break; //case '7'
+
+        case '8':
+          PINin += '8';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+          break; //case '8'
+
+        case '9':
+          PINin += '9';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+          break; //case '9'
+
+        case '0':
+          PINin += '0';
+          //Prints the PINin
+          oled.setCursor(menu4X, menu8Y);
+          oled.setTextColor(OLED_Text_Color);
+          oled.print(PINin);
+          break; //case '0'
+
+        case '*':
+          if (PINin == clkMenu.getPIN())
+            {
+            oled.setCursor(menu4X, menu7Y);
+            oled.setTextColor(OLED_Backround_Color);
+            oled.print("Enter PIN then *\n");
+            oled.print(PINin);
+            //clear PINin
+            PINin = "";
+            //Proceed to next phase of Alarm, Dispense the Pills
+            pillAlarmState = 2;
+      
+            }// END RIGHT INPUT
+          else if (PINin != clkMenu.getPIN())
+            {
+            oled.setCursor(menu4X, menu8Y);
+            oled.setTextColor(OLED_Backround_Color);
+            oled.print(PINin);
+            //clear PINin
+            PINin = "";
+            } // END WRONG INPUT
+          break; //case '*'
+        default:
+          customKey = 'z';
+      } // switch(customKey)
+
+    break; // this will allow looping over prev
+
+    CASE2:
+    case 2:
+    //wait for the cup to be in place VIA the light sensor
+    // if it is in place set [pillAlarmState = 3;]
+    // DAVID INSERT HERE
+
+    //TEST
+    if(customKey == '7'){pillAlarmState = 3;customKey = 'z';}
+    break;
+
+    case 3:
+    //rotate the motor to dispense the pills
+    // DAVID INSERT HERE
+
+    pillAlarmState = 4;
+    //allow fallthrough
+    case 4:
+    // I WILL NEED TO ADD THE SNOOZE FROM THE ALARM CODE
+
+
+    //check if the cup has been removed
+    //snooze when removed
+    // if it is removed, set [pillAlarmState = 5;]
+    // DAVID INSERT HERE
+    
+    //TEST
+    if(customKey == '8'){pillAlarmState = 5;customKey = 'z';}
+    break; //this causes a loop until cup removed
+
+    case 5:
+    //check if cup was returned
+    //true -> turn off Pill ALARM: LED, Speaker
+    // DAVID INSERT HERE
+
+    //increment the pillAlarm counter
+    clkMenu.incPillAlarm();
+    AlarmTripped = 0;
+    OLED_Text_Color = clkMenu.getColor(); //reset Color
+    rtcTime(1);
+    pillAlarmState = 0;
+    //break;
+    //default:
+  }//switch(pillAlarmState)
+  
+  return AlarmTripped;
+}//pillAlarm()
+
+
+// ------------------------------------------------------------------------------------
+
+//use global variables
+uint8_t alarm()
+{
+  customKey = customKeypad.getKey();
+  OLED_Text_Color = OLED_Color_Red; //red for alarm
+  if((now.minute() == clkMenu.getAlarmMM()) || (now.minute() == clkMenu.snoozeAlarmMM)){rtcTime(1);} else{rtcTime(0);}
+  uint8_t AlarmTripped = 1; //0 for false 1 for true
+  switch(alarmState)
+  {
+    case 0:
+    //Turn on the Speaker
+    // DAVID INSERT HERE
+
+    alarmState = 1;
+    // NO break; to allow fall through
+
+    case 1:
+    // Get input from keypad to Snooze alarm, 0 stops, 1 snooze for 1 min, ... 8 snooze for 8 min
+    switch(customKey)
+    {
+      case '1':
+      clkMenu.snoozeAlarmMM = now.minute() + 1;
+      if(clkMenu.snoozeAlarmMM >= 60){clkMenu.snoozeAlarmMM -= 60; clkMenu.snoozeAlarmHH += 1;}
+      alarmState = 2;
+
+      break;
+      case '2':
+      clkMenu.snoozeAlarmMM = now.minute() +  2;
+      if(clkMenu.snoozeAlarmMM >= 60){clkMenu.snoozeAlarmMM -= 60; clkMenu.snoozeAlarmHH += 1;}
+      alarmState = 2;
+
+      break;
+      case '3':
+      clkMenu.snoozeAlarmMM = now.minute() +  3;
+      if(clkMenu.snoozeAlarmMM >= 60){clkMenu.snoozeAlarmMM -= 60; clkMenu.snoozeAlarmHH += 1;}
+      alarmState = 2;
+
+      break;
+      case '4':
+      clkMenu.snoozeAlarmMM = now.minute() +  4;
+      if(clkMenu.snoozeAlarmMM >= 60){clkMenu.snoozeAlarmMM -= 60; clkMenu.snoozeAlarmHH += 1;}
+      alarmState = 2;
+
+      break;
+      case '5':
+      clkMenu.snoozeAlarmMM = now.minute() +  5;
+      if(clkMenu.snoozeAlarmMM >= 60){clkMenu.snoozeAlarmMM -= 60; clkMenu.snoozeAlarmHH += 1;}
+      alarmState = 2;
+
+      break;
+      case '6':
+      clkMenu.snoozeAlarmMM = now.minute() +  6;
+      if(clkMenu.snoozeAlarmMM >= 60){clkMenu.snoozeAlarmMM -= 60; clkMenu.snoozeAlarmHH += 1;}
+      alarmState = 2;
+
+      break;
+      case '7':
+      clkMenu.snoozeAlarmMM = now.minute() +  7;
+      if(clkMenu.snoozeAlarmMM >= 60){clkMenu.snoozeAlarmMM -= 60; clkMenu.snoozeAlarmHH += 1;}
+      alarmState = 2;
+
+      break;
+      case '8':
+      clkMenu.snoozeAlarmMM = now.minute() +  8;
+      if(clkMenu.snoozeAlarmMM >= 60){clkMenu.snoozeAlarmMM -= 60; clkMenu.snoozeAlarmHH += 1;}
+      alarmState = 2;
+
+      break;
+      case '9':
+      clkMenu.snoozeAlarmMM = now.minute() +  9;
+      if(clkMenu.snoozeAlarmMM >= 60){clkMenu.snoozeAlarmMM -= 60; clkMenu.snoozeAlarmHH += 1;}
+      alarmState = 2;
+
+      break;
+      case '0':
+      // normalize the snooze global variables
+      clkMenu.snoozeAlarmHH = clkMenu.getAlarmHH();
+      clkMenu.snoozeAlarmMM = clkMenu.getAlarmMM();
+      alarmState = 2;
+
+      //break;
+      default:
+      customKey = 'z';
+    }
+    break; // this will allow looping over
+
+    case 2:
+    // turn off ALARM: Speaker
+    // DAVID INSERT HERE
+
+    AlarmTripped = 0;
+    OLED_Text_Color = clkMenu.getColor(); //reset Color
+    rtcTime(1);
+    alarmState = 0;
+    //break;
+    //default:
+  }//switch(alarmState)
+  return AlarmTripped;
+}//alarm()
