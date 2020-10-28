d4834f05806dd0e317e6d0b6e41579695110a958
NicholasRC <63579737+NicholasRC@users.noreply.github.com>
2020-10-28 16:20:50 -0400

    Update AlarmTestV2.ino

--- AlarmTestV2/AlarmTestV2.ino
+++ AlarmTestV2/AlarmTestV2.ino
@@ -80,9 +80,9 @@ void loop()
 
   if(speakerTripped == 1){speakerBoom();}
   ledIlluminate(ledTripped);
-  delay(300);
-  if(cupDetected()){digitalWrite(led,HIGH);}
-  else{digitalWrite(led,LOW);}
+  //delay(300);
+  //if(cupDetected()){digitalWrite(led,HIGH);}
+  //else{digitalWrite(led,LOW);}
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
